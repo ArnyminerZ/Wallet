0b92422b17ac842362282ae59b10524a26fc1c9f
Arnau Mora <arnyminer.z@gmail.com>
2022-05-30 20:56:07 +0200

    :sparkles: Added `PassAspect`
    Signed-off-by: Arnau Mora <arnyminer.z@gmail.com>

---
+++ app/src/main/java/com/arnyminerz/wallet/pkpass/data/PassAspect.kt
@@ -0,0 +1,9 @@
+package com.arnyminerz.wallet.pkpass.data
+
+import androidx.annotation.ColorInt
+
+data class PassAspect(
+    @ColorInt val labelColor: Int?,
+    @ColorInt val foregroundColor: Int?,
+    @ColorInt val backgroundColor: Int?,
+)
