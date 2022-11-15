548bd1bb78ca568a4651a02afc1401d982365541
Arnau Mora <arnyminer.z@gmail.com>
2022-11-15 10:10:53 +0100

    :sparkles: Removed preferences
    Signed-off-by: Arnau Mora <arnyminer.z@gmail.com>

--- app/src/main/java/com/arnyminerz/wallet/storage/Preferences.kt
+++ app/src/main/java/com/arnyminerz/wallet/storage/Preferences.kt
@@ -1,9 +1,1 @@
 package com.arnyminerz.wallet.storage
-
-import androidx.datastore.preferences.core.stringPreferencesKey
-
-val SERVER_URL = stringPreferencesKey("server_url")
-
-val CLIENT_ID = stringPreferencesKey("client_id")
-
-val CLIENT_SECRET = stringPreferencesKey("client_secret")
