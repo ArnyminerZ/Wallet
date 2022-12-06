cd75f4690ef1cee81570378ce8709a124837ccef
Arnau Mora <arnyminer.z@gmail.com>
2022-12-06 01:54:09 +0100

    Updated dashboard design
    Signed-off-by: Arnau Mora <arnyminer.z@gmail.com>

--- app/src/main/java/com/arnyminerz/wallet/ui/pages/firefly/Dashboard.kt
+++ app/src/main/java/com/arnyminerz/wallet/ui/pages/firefly/Dashboard.kt
@@ -1,92 +1,202 @@
 package com.arnyminerz.wallet.ui.pages.firefly
 
 import android.accounts.Account
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.rounded.Login
-import androidx.compose.material.icons.rounded.Logout
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material3.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
+import androidx.compose.runtime.*
 import androidx.compose.runtime.livedata.observeAsState
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.arnyminerz.wallet.R
+import com.arnyminerz.wallet.database.data.FireflyTransaction
+import com.arnyminerz.wallet.database.data.TransactionType
 import com.arnyminerz.wallet.model.MainViewModel
-import com.arnyminerz.wallet.ui.elements.SummaryEntry
+import com.arnyminerz.wallet.ui.components.BalanceCard
+import com.arnyminerz.wallet.ui.components.TransactionCard
+import com.arnyminerz.wallet.ui.theme.colorGreen
+import com.arnyminerz.wallet.ui.theme.colorRed
+import com.arnyminerz.wallet.ui.utils.isScrolledToEnd
+import kotlinx.coroutines.flow.distinctUntilChanged
+import timber.log.Timber
+import java.text.SimpleDateFormat
+import java.util.*
+
+private const val VIEW_DAILY = 0
+private const val VIEW_WEEKLY = 1
+private const val VIEW_MONTHLY = 2
+private const val VIEW_YEARLY = 3
 
 @Composable
 @ExperimentalMaterial3Api
+@ExperimentalFoundationApi
 fun FireflyDashboard(viewModel: MainViewModel = viewModel(), account: Account) {
-    val summary by remember { viewModel.getSummary(account) }.observeAsState()
-    val transactions by remember { viewModel.getTransactions(account, 7) }.observeAsState()
-    val accounts by remember { viewModel.storedAccounts }.observeAsState()
+    val context = LocalContext.current
 
-    Column {
-        summary?.let { sum ->
-            SummaryEntry(
-                sum.balance,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(horizontal = 8.dp, vertical = 4.dp)
-            )
-        } ?: Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-            CircularProgressIndicator()
-        }
-        Card(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(8.dp),
-        ) {
-            Text(
-                stringResource(R.string.dashboard_accounts_balance),
-                style = MaterialTheme.typography.titleLarge,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(horizontal = 8.dp, vertical = 4.dp),
-            )
-            accounts?.filter { it.type == "asset" }?.forEach { account ->
-                Row(
-                    verticalAlignment = Alignment.CenterVertically,
-                    modifier = Modifier
-                        .padding(vertical = 16.dp),
-                ) {
-                    Text(
-                        text = account.name,
-                        modifier = Modifier
-                            .weight(1f)
-                            .padding(start = 16.dp),
-                        style = MaterialTheme.typography.titleMedium,
-                    )
-                    Text(
-                        account.currency.format(account.balance),
-                        modifier = Modifier
-                            .padding(end = 16.dp),
-                        style = MaterialTheme.typography.labelLarge,
-                    )
+    val scrollState = rememberLazyListState()
+
+    val summary by remember { viewModel.getSummary(account) }.observeAsState()
+    var transactions by remember { mutableStateOf(emptyList<Pair<String, List<FireflyTransaction>>>()) }
+    var selectedView by remember { mutableStateOf(0) }
+    var offset by remember { mutableStateOf(0) }
+    var reachedEnd by remember { mutableStateOf(false) }
+
+    fun updateTransactions(list: List<FireflyTransaction> = transactions.map { it.second }.flatten()) {
+        transactions = list.groupBy {
+            SimpleDateFormat(
+                when(selectedView) {
+                    VIEW_DAILY -> "EEE, MMM d, yyyy"
+                    VIEW_WEEKLY -> "'" + context.getString(R.string.dashboard_group_week_number) + "' w, yyyy"
+                    VIEW_MONTHLY -> "MMMM yyyy"
+                    else -> "yyyy"
+                },
+                Locale.getDefault(),
+            ).format(it.date)
+        }.toList()
+    }
+
+    LaunchedEffect(transactions) {
+        Timber.d("Getting transactions for today")
+        viewModel.getTransactions(
+            account,
+            start = Calendar.getInstance().time,
+            end = Calendar.getInstance()
+                .apply {
+                    add(Calendar.DATE, 1)
                 }
-            } ?: Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-                CircularProgressIndicator()
+                .time,
+        ) {
+            val all = transactions
+                .map { it.second }
+                .flatten()
+                .toMutableList()
+            updateTransactions(all.apply { addAll(it) })
+        }
+    }
+
+    LaunchedEffect(scrollState) {
+        snapshotFlow { scrollState.isScrolledToEnd() }
+            .distinctUntilChanged()
+            .collect {
+                while (scrollState.isScrolledToEnd() && !reachedEnd) {
+                    offset++
+                    Timber.d("Getting transactions for $offset day(s) ago")
+                    viewModel.getTransactions(
+                        account,
+                        start = Calendar.getInstance()
+                            .apply {
+                                add(Calendar.DATE, -offset)
+                            }
+                            .time,
+                        end = Calendar.getInstance()
+                            .apply {
+                                add(Calendar.DATE, -offset)
+                            }
+                            .time,
+                    ) { tr ->
+                        if (tr.isEmpty())
+                            reachedEnd = true
+                        else {
+                            val all = transactions
+                                .map { it.second }
+                                .flatten()
+                                .toMutableList()
+                            updateTransactions(all.apply { addAll(tr) })
+                        }
+                    }
+                }
+            }
+    }
+
+    LazyColumn(state = scrollState) {
+        item {
+            summary?.let { sum -> BalanceCard(sum, modifier = Modifier.padding(horizontal = 8.dp)) }
+                ?: Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                    CircularProgressIndicator()
+                }
+        }
+        stickyHeader {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 4.dp)
+                    .background(MaterialTheme.colorScheme.background)
+            ) {
+                FilterChip(
+                    selected = selectedView == VIEW_DAILY,
+                    onClick = { selectedView = VIEW_DAILY; updateTransactions() },
+                    label = { Text(stringResource(R.string.dashboard_sort_daily)) },
+                    modifier = Modifier.padding(horizontal = 4.dp),
+                )
+                FilterChip(
+                    selected = selectedView == VIEW_WEEKLY,
+                    onClick = { selectedView = VIEW_WEEKLY; updateTransactions() },
+                    label = { Text(stringResource(R.string.dashboard_sort_weekly)) },
+                    modifier = Modifier.padding(horizontal = 4.dp),
+                )
+                FilterChip(
+                    selected = selectedView == VIEW_MONTHLY,
+                    onClick = { selectedView = VIEW_MONTHLY; updateTransactions() },
+                    label = { Text(stringResource(R.string.dashboard_sort_monthly)) },
+                    modifier = Modifier.padding(horizontal = 4.dp),
+                )
+                FilterChip(
+                    selected = selectedView == VIEW_YEARLY,
+                    onClick = { selectedView = VIEW_YEARLY; updateTransactions() },
+                    label = { Text(stringResource(R.string.dashboard_sort_yearly)) },
+                    modifier = Modifier.padding(horizontal = 4.dp),
+                )
             }
         }
-        Text("Last 7 transactions:")
-        transactions?.forEach { transaction ->
-            ListItem(
-                headlineText = { Text(transaction.amountString + " - " + transaction.description) },
-                leadingContent = {
-                    Icon(
-                        Icons.Rounded.Login.takeIf { transaction.type == "deposit" } ?: Icons.Rounded.Logout,
-                        // TODO: Localize
-                        "Income",
-                    )
-                },
-            )
-        } ?: Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-            CircularProgressIndicator()
+        items(transactions) { (date, transactions) ->
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 8.dp)
+                    .padding(vertical = 4.dp),
+            ) {
+                val sum = transactions
+                    .filter { arrayOf(TransactionType.DEPOSIT, TransactionType.WITHDRAWAL).contains(it.type) }
+                    .sumOf { it.amount * (if (it.type == TransactionType.DEPOSIT) 1 else -1) }
+                Text(
+                    date,
+                    modifier = Modifier
+                        .weight(1f),
+                    style = MaterialTheme.typography.labelLarge,
+                    fontWeight = FontWeight.SemiBold,
+                    fontSize = 18.sp,
+                )
+                Text(
+                    sum.let { transactions.firstOrNull()?.currency?.format(it) } ?: "",
+                    modifier = Modifier,
+                    style = MaterialTheme.typography.labelLarge,
+                    fontWeight = FontWeight.SemiBold,
+                    fontSize = 18.sp,
+                    color = when {
+                        sum > 0 -> colorGreen
+                        sum < 0 -> colorRed
+                        else -> Color.Unspecified
+                    }
+                )
+            }
+            for (transaction in transactions) {
+                TransactionCard(
+                    transaction = transaction, modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 8.dp, vertical = 4.dp)
+                )
+            }
         }
     }
 }
